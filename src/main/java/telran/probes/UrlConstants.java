7cf939dd22c5b414211c61438134e26932db8c13
oljas <olja.smirnov93@gmail.com>
2024-03-02 11:06:24 +0200

    HW#74 UrlConstants

---
+++ src/main/java/telran/probes/UrlConstants.java
@@ -0,0 +1,8 @@
+package telran.probes;
+
+public interface UrlConstants {
+	String RANGE_PROVIDER = "range/sensor/";
+	String EMAILS_PROVIDER = "emails/sensor/";
+	String SENSOR_ID = "sensorId";
+
+}
